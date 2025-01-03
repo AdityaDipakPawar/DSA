37c395e63cb74ce954f1b57f2429599483d1e749
AdityaDipakPawar <adityadpawar71@gmail.com>
2025-01-03 13:14:36 +0530

    Bubble Sort

---
+++ Bubble Sort/src/com/aditya/Main.java
@@ -0,0 +1,33 @@
+package com.aditya;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSort(int[] arr){
+        boolean swapped;
+        // run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            // for each step, max item will come at the last respective index
+            for (int j = 1; j < arr.length-i; j++) {
+                //swap if the items is smaller than the previous item
+                if(arr[j] < arr[j-1]){
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            //if you did not swap for a particular value of i, it means the array is sorted hence stop
+            if(!swapped){//swapped == false
+                break;
+            }
+        }
+    }
+}
