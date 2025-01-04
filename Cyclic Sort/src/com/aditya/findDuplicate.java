f275ed7140f96ba627ae5f2a98cf5c7878922fff
AdityaDipakPawar <adityadpawar71@gmail.com>
2025-01-04 17:10:23 +0530

    Cyclic Sort

---
+++ Cyclic Sort/src/com/aditya/findDuplicate.java
@@ -0,0 +1,56 @@
+package com.aditya;
+
+public class findDuplicate {
+//    public int findDuplicate(int[] nums) {
+//        int i = 0;
+//        while (i < nums.length) {
+//            int correct = nums[i] - 1;
+//            if (nums[i] < nums.length && nums[i] != nums[correct]) {
+//                swap(nums, i, correct);
+//            } else {
+//                i++;
+//            }
+//        }
+//
+//        for (int index = 0; index < nums.length; index++) {
+//            if (nums[index] != index + 1) {
+//                return nums[index];
+//            }
+//        }
+//
+//        return nums.length;
+//    }
+
+    public int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+
+            if (nums[i] != i + 1) {
+                int correct = nums[i] - 1;
+                if (nums[i] != nums[correct]) {
+                    swap(nums, i, correct);
+                } else {
+                    return nums[i];
+                }
+            } else {
+                i++;
+            }
+
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return nums[index];
+            }
+        }
+
+        return nums.length;
+    }
+
+
+    public void swap(int[] arr, int first, int last) {
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+}
